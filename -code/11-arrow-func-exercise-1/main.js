6b109fadcebb1903f48f20cd82b4defcd41d3bbd
Vassil Alexandrov Guenov <vassil.alexandrov@sofia.stanga.net>
2017-01-18 14:35:46 +0200

    HTML fragments with template literals

--- -code/11-arrow-func-exercise-1/main.js
+++ -code/11-arrow-func-exercise-1/main.js
@@ -1,15 +1,31 @@
+// // Select all the list items on the page and convert to array
+// const list = Array.from(document.querySelectorAll('[data-time]'));
+// // Filter for only the elements that contain the word 'flexbox'
+// const flexList = list.filter(li => li.textContent.toLowerCase().includes('flexbox'));
+// // map down to a list of time strings
+// const flexTime = flexList.map(li => li.attributes['data-time'].value);
+// // function to convert minutes to seconds
+// const minToSecs = li => Number(li.slice(0, li.indexOf(':'))) * 60 + Number(li.slice(li.indexOf(':') + 1));
+// // map to an array of seconds
+// const flexTimeSec = flexTime.map(li => minToSecs(li));
+// // reduce to get total
+// const totalflexTimeSec = flexTimeSec.reduce((total, num) => total + num);
+// console.log(`${totalflexTimeSec} sec`);
+// // 🔥 This can also be done in a single .reduce(), but we're practicing arrow functions here, so chain them!
+
 // Select all the list items on the page and convert to array
-const list = Array.from(document.querySelectorAll('li'));
+const list = Array.from(document.querySelectorAll('[data-time]'));
 // Filter for only the elements that contain the word 'flexbox'
-const flexList = list.filter(li => li.innerText.toLowerCase().includes('flexbox'));
-// map down to a list of time strings
-console.log(flexList[0].attributes['data-time'].value);
-const flexTime = flexList.map(i => flexList[i].attributes['data-time'].value);
-console.log(flexTime);
-// function to convert minutes to seconds
-
-// map to an array of seconds
-
-// reduce to get total
-
-// 🔥 This can also be done in a single .reduce(), but we're practicing arrow functions here, so chain them!
+const filtered = list
+  .filter(li => li.textContent.includes('Flexbox'))
+  // map down to a list of time strings
+  .map(li => li.dataset.time)
+  // map to an array of seconds
+  .map(timeCode => {
+    const parts = timeCode.split(':').map(part => parseFloat(part));
+    return parts[0] * 60 + parts[1];
+  })
+  // reduce to get total
+  .reduce((runningTotal, seconds) => runningTotal + seconds, 0);
+  
+console.log(filtered);
