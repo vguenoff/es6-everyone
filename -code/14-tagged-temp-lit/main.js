acabd332e74cf5b23c0a5106614b59c64db84575
Vassil Alexandrov Guenov <vassil.alexandrov@sofia.stanga.net>
2017-01-19 19:01:15 +0200

    tagged-templates finished

--- -code/14-tagged-temp-lit/main.js
+++ -code/14-tagged-temp-lit/main.js
@@ -1,15 +1,16 @@
 function highlight(strings, ...values) {
-    let str = '';
-    strings.forEach((string, i) => {
-        str += `${string} <span contenteditable class='hl'>${values[i] || ''}</span>`;
-    });
-    return str;
+  let str = '';
+
+  strings.forEach((string, i) => {
+    str += `${string} <span contenteditable class='hl'>${values[i] || ''}</span>`;
+  });
+
+  return str;  
 }
 
 const name = 'Snickers';
 const age = 100;
+const sentence =  highlight`My dog's name is ${name} and he is ${age} years old.`;
 
-
-const sentence = highlight`My dog name is ${name} and he is ${age} old.`;
 document.body.innerHTML = sentence;
-
+console.log(sentence);
--- -code/15-tagged-temp-exercise/main.js
+++ -code/15-tagged-temp-exercise/main.js
@@ -1,28 +1,30 @@
 const dict = {
-    HTML: 'Hypertext Markup Language',
-    CSS: 'Cascading Style Sheets',
-    JS: 'JavaScript'
+  HTML: 'Hyper Text Markup Language',
+  CSS: 'Cascading Style Sheets',
+  JS: 'JavaScript'
 };
 
-function addAbbreviations(strings, ...values) {
-    const abbreviated = values.map(value => {
-        if (dict[value]) {
-            return `<abbr title="${dict[value]}">${value}</abbr>`;
-        }
-        return value;
-    });
+function addAbbreviation(strings, ...values) {
+  const abbreviated = values.map(value => {
+    if(dict[value]) {
+      return `<abbr title="${dict[value]}">${value}</abbr>`;
+    }
+    return value;
+  });
 
-    return strings.reduce((sentence, string, i) => {
-        return `${sentence}${string}${abbreviated[i] || ''}`;
-    }, '');
-
+  return strings.reduce((sentence, string, i) => {
+    return `${sentence}${string}${abbreviated[i] || ''}`;
+  }, '');
 }
+
 
 const first = 'Wes';
 const last = 'Bos';
-const sentence = addAbbreviations`Hello, my name is ${first} ${last} and I love to code ${'HTML'}, ${'CSS'} and ${'JS'}.`;
+
+const sentence = addAbbreviation`Hello my name is ${first} ${last} and I love to code ${'HTML'}, ${'CSS'}, ${'JS'}.`;
 
 const bio = document.querySelector('.bio');
 const p = document.createElement('p');
 p.innerHTML = sentence;
 bio.appendChild(p);
+
