e3b89d0e2aeca9cf787fec053735c2feaf74cf83
Vassil Alexandrov Guenov <vassil.alexandrov@sofia.stanga.net>
2017-02-01 18:30:31 +0200

    s

---
+++ -code/28-spread-operator-intro/main.js
@@ -0,0 +1,10 @@
+const featured = ['Deep Dish', 'Peperoni', 'Hawaiian'];
+const specality = ['Meatzaa', 'Spicy Mama', 'Margherita'];
+const pizza = featured.concat(specality);
+console.log(pizza);
+
+const deepDish = {
+  pizzaName: 'Deep Dish',
+  size: 'Medium',
+  ingredients: ['Marinara', 'Italian Sausage', 'Dough', 'Cheese']
+};
